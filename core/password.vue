4f3e74bce0424a604836550856a1bf5dcc8d0fb7
crossjs <liwenfu@crossjs.com>
2017-01-06 22:00:36 +0800

    initial

---
+++ core/password.vue
@@ -0,0 +1,59 @@
+<template>
+  <span class="c-password">
+    <c-textfield
+      :type="showPassword ? 'text' : 'password'"
+      :value="value"
+      v-bind="attrs"
+      @change="onChange"></c-textfield>
+    <c-icon class="c-password-toggle"
+      :class="{active: showPassword}"
+      v-tap @tap.native="toggle">eye</c-icon>
+  </span>
+</template>
+
+<script>
+import CTextfield from './textfield'
+import CIcon from './icon'
+import mField from './mixins/field'
+import tap from '../directives/tap'
+
+export default {
+  mixins: [mField],
+
+  props: {
+    attrs: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+
+  data () {
+    return {
+      showPassword: false
+    }
+  },
+
+  methods: {
+    toggle () {
+      this.showPassword = !this.showPassword
+    },
+    onChange (value) {
+      // override
+      this.$emit('change', value)
+    }
+  },
+
+  components: {
+    CTextfield,
+    CIcon
+  },
+
+  directives: {
+    tap
+  }
+}
+</script>
+
+<style src="./styles/password"></style>
